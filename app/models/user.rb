96aafa4f50dd49233191330d6f3a8268af93607a
ciscolegrand <cisco.glez@gmail.com>
2021-08-08 20:55:58 +0200

    [refactor] reordering code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include PermissionLevel
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
