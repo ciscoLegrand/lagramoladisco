fec9a8f9fcd89212cc82c082e0b7907c24e7a361
ciscolegrand <cisco.glez@gmail.com>
2021-08-19 22:27:36 +0200

    [reafactor] notification component 2.0

--- app/components/notification_component.rb
+++ app/components/notification_component.rb
@@ -1,37 +1,14 @@
 # frozen_string_literal: true
 
 class NotificationComponent < ViewComponent::Base
-  def initialize(type:, data:)
-    @type = type
-    @data = prepare_data(data)
-    @icon_class = icon_class
-    @color_class = color_class
-  end
+  attr_reader :data, :type, :icon, :color_class
 
-  def icon_class
-    case @type
-    when 'notice'
-      'fa-check-square has-text-success'
-    when 'error'
-      'fa-ban has-text-danger'
-    when 'alert'
-      'fa-exclamation-triangle has-text-warning'
-    else
-      'fa-info-circle has-text-info'
-    end
-  end
-  
-  def color_class
-    case @type
-    when 'notice'
-      'success'
-    when 'error'
-      'error'
-    when 'alert'
-      'alert'
-    else
-      'info'
-    end
+  def initialize(notification:)
+    
+    @type = notification[0]
+    @data = prepare_data(notification[1])
+    @icon = icon
+    @color_class = color_class
   end
 
   private
@@ -39,10 +16,40 @@ class NotificationComponent < ViewComponent::Base
   def prepare_data(data)
     case data
     when Hash
-      @data
-    else  
-      {title: data}
+      data
+    else
+      { title: data }
     end
+  end
+
+  def icon
+    case @type
+    when 'notice'
+      inline_svg_tag('icons/ico-check-circle.svg', class: 'icon is-medium is-success')
+    when 'error'
+      inline_svg_tag('icons/ico-ban.svg', class: 'icon is-medium is-danger')
+    when 'alert'
+      inline_svg_tag('icons/ico-exclamation-triangle.svg', class: 'icon is-medium is-warning')
+    else
+      inline_svg_tag('icons/ico-information-circle.svg', class: 'icon is-medium is-warning')
+    end
+  end
+  
+  def color_class
+    case @type
+    when 'notice'
+      'success has-text-success'
+    when 'error'
+      'error has-text-danger'
+    when 'alert'
+      'alert has-text-warning'
+    else
+      'notice has-text-info'
+    end
+  end
+
+  def render?
+    data.present?
   end
 
 end
